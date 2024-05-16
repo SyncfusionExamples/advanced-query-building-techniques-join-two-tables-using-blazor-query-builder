8bb3c4398a157d2ccacb1e39b976495b045c36b0
Satheeskumar S <satheeskumars@syncfusion.com>
2024-05-16 10:38:19 +0530

    865189: Query Builder Join sample

---
+++ SyncfusionBlazorApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using Fluxor;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Hosting;
+using Syncfusion.Blazor;
+using SyncfusionBlazorApp.Data;
+using Blazored.SessionStorage;
+using Microsoft.JSInterop;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<IJSInProcessRuntime>(services => (IJSInProcessRuntime)services.GetRequiredService<IJSRuntime>());
+builder.Services.AddFluxor(options =>
+{
+    options.ScanAssemblies(typeof(Program).Assembly);
+});
+builder.Services.AddBlazoredSessionStorage();
+
+var app = builder.Build();
+//Register Syncfusion license https://help.syncfusion.com/common/essential-studio/licensing/how-to-generate
+//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR LICENSE KEY");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ SyncfusionBlazorApp/Store/SetValueAction.cs
@@ -0,0 +1,8 @@
+﻿using SyncfusionBlazorApp.Store.ValueUseCase;
+
+namespace SyncfusionBlazorApp.Store
+{
+    public record EventValueClicked(string[] Items);
+    public record SetValueAction(string[] Items);
+    public record LoadValuesFromStorageAction(ValueState storedState);
+}
